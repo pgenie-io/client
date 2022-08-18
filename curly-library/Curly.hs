216a91876f6e82c01bf41b8932a8b79937bf9095
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-08-18 21:14:55 +0300

    Sketch

--- curly-library/Curly.hs
+++ curly-library/Curly.hs
@@ -13,36 +13,62 @@ import qualified Network.CURL730 as Curl
 
 -- * Handle
 
-newtype Handle
-  = Handle (TQueue Curl.CURL)
+data Handle = Handle
+  { handleQueue :: !(TQueue Curl.CURL),
+    handleTakenCounter :: !(TVar Int)
+  }
 
--- | Acquire handle pool,
--- which can be used to execute sessions.
+-- | Acquire a handle which can be reused for execution of sessions.
+--
+-- Essentially it's a pool of Curl Easy handles.
 acquireHandle :: Int -> IO Handle
 acquireHandle =
+  error "TODO"
+
+useHandle :: Handle -> Op a -> IO (Either Curl.CURLE a)
+useHandle Handle {..} (Op runOp) =
+  error "TODO"
+
+useHandleHappily :: Handle -> Op a -> IO a
+useHandleHappily =
   error "TODO"
 
 -- * Op
 
 newtype Op a
-  = Op (Curl.CURL -> IO (Either Curl.CURLE a))
+  = Op (Curl.CURL -> IO (Either OpErr a))
+
+data OpErr
+  = CurlOpErr Curl.CURLE
+  | BodyParserOpErr Text
 
 post ::
   Url ->
   [(Text, Text)] ->
   ByteString ->
-  HeadersParser (BodyParser a) ->
-  Op a
-post =
-  error "TODO"
+  HeadersParser headers ->
+  BodyParser body ->
+  Op (headers, body)
+post Url {..} headers body _ (BodyParser initWriteFunction) =
+  Op $ \curl -> runExceptT $ do
+    (writeFunction, readBodyResult) <- lift initWriteFunction
+    lift $
+      Curl.curl_easy_setopt
+        curl
+        [ Curl.CURLOPT_WRITEFUNCTION . Just $ writeFunction
+        ]
+    headers <- error "TODO"
+    ExceptT $ catch (Right <$> Curl.curl_easy_perform curl) (return . Left . CurlOpErr)
+    body <- ExceptT $ first BodyParserOpErr <$> readBodyResult
+    return (headers, body)
 
 -- * Url
 
-data Url
-  = Url
-      !Bool
-      !(BVec Text)
-      !Path.Path
+data Url = Url
+  { urlSecure :: !Bool,
+    urlDomain :: !(BVec Text),
+    urlPath :: !Path.Path
+  }
 
 -- * HeadersParser
 
@@ -50,4 +76,5 @@ data HeadersParser a
 
 -- * BodyParser
 
-data BodyParser a
+newtype BodyParser a
+  = BodyParser (IO (ByteString -> IO Curl.CURL_write_response, IO (Either Text a)))
