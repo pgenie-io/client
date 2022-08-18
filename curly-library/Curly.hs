ec9de9152666aeb38407e09b06b89849137812be
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-08-18 20:37:06 +0300

    Extend

--- curly-library/Curly.hs
+++ curly-library/Curly.hs
@@ -14,11 +14,11 @@ import qualified Network.CURL730 as Curl
 -- * Handle
 
 newtype Handle
-  = Handle Curl.CURL
+  = Handle (TQueue Curl.CURL)
 
--- | Acquire reusable handle,
+-- | Acquire handle pool,
 -- which can be used to execute sessions.
-acquireHandle :: IO Handle
+acquireHandle :: Int -> IO Handle
 acquireHandle =
   error "TODO"
 
@@ -26,3 +26,28 @@ acquireHandle =
 
 newtype Op a
   = Op (Curl.CURL -> IO (Either Curl.CURLE a))
+
+post ::
+  Url ->
+  [(Text, Text)] ->
+  ByteString ->
+  HeadersParser (BodyParser a) ->
+  Op a
+post =
+  error "TODO"
+
+-- * Url
+
+data Url
+  = Url
+      !Bool
+      !(BVec Text)
+      !Path.Path
+
+-- * HeadersParser
+
+data HeadersParser a
+
+-- * BodyParser
+
+data BodyParser a
