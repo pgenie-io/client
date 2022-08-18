42c0acdec144dc46de0eaff83b5c6b6147a647ea
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-08-18 20:07:25 +0300

    Sketch curly

--- curly-library/Curly.hs
+++ curly-library/Curly.hs
@@ -1,5 +1,28 @@
-module Curly where
+module Curly
+  ( -- * Handle
+    Handle,
+    acquireHandle,
+
+    -- * Op
+  )
+where
 
 import qualified Coalmine.EvenSimplerPaths as Path
-import Coalmine.Prelude hiding (Op, Version)
-import Network.CURL730
+import Coalmine.Prelude hiding (Handle, Op, Version)
+import qualified Network.CURL730 as Curl
+
+-- * Handle
+
+newtype Handle
+  = Handle Curl.CURL
+
+-- | Acquire reusable handle,
+-- which can be used to execute sessions.
+acquireHandle :: IO Handle
+acquireHandle =
+  error "TODO"
+
+-- * Op
+
+newtype Op a
+  = Op (Curl.CURL -> IO (Either Curl.CURLE a))
